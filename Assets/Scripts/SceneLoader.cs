2ea2b12af90b70861b8f9103571de99920ebe1a0
Vinay Mukkawar <69383431+vinaymukkawar@users.noreply.github.com>
2022-02-25 16:05:48 +0530

    Initial Commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrentSceneIndex + 1);
+
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
